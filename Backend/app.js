a2448ba98faa3282fe663ec35cfdaca8d2680423
MarcosD00 <olito007.23@gmail.com>
2024-02-08 17:42:06 -0600

    last update

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const routes = require('./routes');
+
+// ...
+
+app.use(routes); // Connect all the routes
+
+module.exports = app;
